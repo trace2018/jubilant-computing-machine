d1004e15fedcf0318376c56d6aa0f4fe2216e3f9
trace2018 <chuakenneth2018@gmail.com>
2019-05-03 19:50:25 +0800

    Create the top level frame

---
+++ abq_data_entry/application.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+class Application(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title("ABQ Data Entry Application")
+        self.resizable(width=False, height=False)
